be6bbd1ab31e9e5d2c1120ed2fac1da3f7a6430c
Kenshiro <sinisastanic@outlook.com>
2022-02-10 01:37:58 +0100

    extracted human specific converting

---
+++ modules/HumanConverter.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+OBJECT_UUID = 'temporalId'
+
+age_converter = {
+    'adult': 0,
+    'child': 1
+}
+
+
+def convert_human(row, ridden_bicycles: pd.DataFrame, unique_ids):
+    human_uuid = row[OBJECT_UUID]
+    # ridden_bicycles = ridden_bicycles.set_index('human_id')
+    helmet = None
+    if human_uuid in ridden_bicycles['human_id'].tolist():
+        helmet = 1 if row['wears_helmet'] else 0
+
+    return {
+        'HUMAN_ID': unique_ids['HUMAN'].loc[human_uuid],
+        'AGE': age_converter[row['age']],
+        'WEARS_HELMET': helmet
+    }
+
+
+
